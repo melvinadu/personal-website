96ba17d27c10174049abf0afc0169db3d21d0591
Melvin <contactmelvinadu@gmail.com>
2022-07-16 15:52:38 -0400

    second commit

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -60,13 +60,12 @@ const Hero = () => {
 
   const one = <h1>Hi, my name is</h1>;
   const two = <h2 className="big-heading">Melvin Adu.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const three = <h3 className="big-heading">I build and then scale things on the web.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on making commerce better for everyone
-        at{' '}
+        Melvin Adu is an Entrepreneur who specializes in solving complex problems with technology
+        and data. Currently, I’m focused on Making Commerce Better for Everyone at{' '}
         <a href="https://shopify.com/" target="_blank" rel="noreferrer">
           Shopify
         </a>
