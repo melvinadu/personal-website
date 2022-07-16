549c27f37e5e9205f6149458add26c440fe9ece3
Melvin <contactmelvinadu@gmail.com>
2022-07-16 15:59:43 -0400

    remove course content

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -73,17 +73,17 @@ const Hero = () => {
       </p>
     </>
   );
-  const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
-    </a>
-  );
+  // const five = (
+  //   <a
+  //     className="email-link"
+  //     href="https://www.newline.co/courses/build-a-spotify-connected-app"
+  //     target="_blank"
+  //     rel="noreferrer">
+  //     Check out my course!
+  //   </a>
+  // ); TO-DO - add course
 
-  const items = [one, two, three, four, five];
+  const items = [one, two, three, four];
 
   return (
     <StyledHeroSection>
